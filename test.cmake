aaeffac50a49981c7bf964b4948a1bcbd59601ca
MINGUOLIU <66464677+MINGUOLIU@users.noreply.github.com>
2023-08-17 21:37:17 +0800

    提交cmake测试

---
+++ test.cmake
@@ -0,0 +1,13 @@
+# Load the main CMakeLists.txt
+#include(${CMAKE_SOURCE_DIR}/CMakeLists.txt)
+
+# Add your testing code here
+message("Running test for MyTinySTL")
+
+# Check project version
+message("Project version: ${MyTinySTL_VERSION}")
+
+# Check compiler flags
+#message("Compiler flags: ${CMAKE_CXX_FLAGS}")
+
+# Add more testing code if needed
